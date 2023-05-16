dd6754d88e11c11d0c7fbde53c03fa8ae4eb6eaa
Henrique Botelho <henriquebotelho.imp@gmail.com>
2023-05-16 11:50:57 -0300

    datagrid

--- src/Pages/Compras/index.jsx
+++ src/Pages/Compras/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import imagemCantina from "../../assets/fundo.png";
+import { DataGrid } from "@mui/x-data-grid";
 
 import Header from "../../components/Header";
 
@@ -9,9 +10,34 @@ function Compras() {
     backgroundSize: "cover"
   }
 
+  const columns = [
+    {field: "cliente", headerName: "Cliente", width: 200, editable: true},
+    {field: "detalhe", headerName: "Detalhes", width: 100},
+    {field: "datetime", headerName: "Data/Hora", width: 100},
+    {field: "total", headerName: "Total", type: "number", width: 100},
+  ]
+
+  const rows = [
+    {id: 1, cliente: "Henrique", detalhe: "Pão de queijo", datetime: "16/05/2023", total: 3},
+  ]
+
   return(
     <div style={backgroundImageStyle} className="h-screen w-screen flex justify-center items-center">
       <Header />
+      <div className="container bg-white rounded h-96">
+        <DataGrid 
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel : {page: 0, pageSize: 10}
+            }
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </div>
     </div>
   )
 }
