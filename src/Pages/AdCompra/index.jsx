a452e78930c445bcc6fd6f87b4377978ba920a08
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-05-17 22:17:14 -0300

    Update index.jsx

--- src/Pages/AdCompra/index.jsx
+++ src/Pages/AdCompra/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
 import { Autocomplete } from "@mui/material";
 
+// Icones do react icons
 import { BiArrowBack } from "react-icons/bi";
 import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 import { BsArrowRightCircleFill } from "react-icons/bs";
@@ -11,46 +12,49 @@ import imagemCantina from "../../assets/fundo.png";
 
 import Header from "../../components/Header";
 
-
 function AdCompra() {
   const backgroundImageStyle = {
     backgroundImage: `url('${imagemCantina}')`,
     backgroundSize: "cover",
   };
 
+  // Clientes disponíveis (teste)
   let clientes = ["Henrique", "Heitor", "Guilherme"];
 
+  // Linhas produtos (teste)
   const rows = [
-    {id: 1, nome: "Pão de queijo", preco: 3},
-    {id: 2, nome: "Pão de queijo", preco: 3},
-    {id: 3, nome: "Pão de queijo", preco: 3},
-    {id: 4, nome: "Pão de queijo", preco: 3},
-    {id: 5, nome: "Pão de queijo", preco: 3},
-    {id: 6, nome: "Pão de queijo", preco: 3},
-    {id: 7, nome: "Pão de queijo", preco: 3},
-    {id: 8, nome: "Pão de queijo", preco: 3},
-    {id: 9, nome: "Pão de queijo", preco: 3},
-    {id: 10, nome: "Pão de queijo", preco: 3},
-    {id: 11, nome: "Pão de queijo", preco: 3},
-    {id: 12, nome: "Pão de queijo", preco: 3},
-    {id: 13, nome: "Pão de queijo", preco: 3},
-    {id: 14, nome: "Pão de queijo", preco: 3},
-    {id: 15, nome: "Pão de queijo", preco: 3},
-    {id: 16, nome: "Pão de queijo", preco: 3},
-    {id: 17, nome: "Pão de queijo", preco: 3},
-    {id: 18, nome: "Pão de queijo", preco: 3},
-    {id: 19, nome: "Pão de queijo", preco: 3},
-    {id: 20, nome: "Pão de queijo", preco: 3},
-  ]
+    { id: 1, nome: "Pão de queijo", preco: 3 },
+    { id: 2, nome: "Pão de queijo", preco: 3 },
+    { id: 3, nome: "Pão de queijo", preco: 3 },
+    { id: 4, nome: "Pão de queijo", preco: 3 },
+    { id: 5, nome: "Pão de queijo", preco: 3 },
+    { id: 6, nome: "Pão de queijo", preco: 3 },
+    { id: 7, nome: "Pão de queijo", preco: 3 },
+    { id: 8, nome: "Pão de queijo", preco: 3 },
+    { id: 9, nome: "Pão de queijo", preco: 3 },
+    { id: 10, nome: "Pão de queijo", preco: 3 },
+    { id: 11, nome: "Pão de queijo", preco: 3 },
+    { id: 12, nome: "Pão de queijo", preco: 3 },
+    { id: 13, nome: "Pão de queijo", preco: 3 },
+    { id: 14, nome: "Pão de queijo", preco: 3 },
+    { id: 15, nome: "Pão de queijo", preco: 3 },
+    { id: 16, nome: "Pão de queijo", preco: 3 },
+    { id: 17, nome: "Pão de queijo", preco: 3 },
+    { id: 18, nome: "Pão de queijo", preco: 3 },
+    { id: 19, nome: "Pão de queijo", preco: 3 },
+    { id: 20, nome: "Pão de queijo", preco: 3 },
+  ];
 
+  // Dados a serem enviados para o backend
   const [itens, setItens] = useState([]);
   const [cliente, setCliente] = useState(clientes[0]);
   const [total, setTotal] = useState(0);
 
+  // Funções para manipulação dos itens
   const adicionaProduto = (linha) => {
     const novosItens = [...itens];
     let adicionado = false;
-    novosItens.map(produto => {
+    novosItens.map((produto) => {
       if (produto.id == linha.id) {
         adicionado = true;
       }
@@ -62,12 +66,11 @@ function AdCompra() {
       id: linha.id,
       nome: linha.nome,
       preco: linha.preco,
-      quantidade: 1
-    }
+      quantidade: 1,
+    };
     novosItens.push(item);
     setItens(novosItens);
-    
-  }
+  };
 
   const removeProduto = (linha) => {
     const novosItens = [...itens];
@@ -77,8 +80,7 @@ function AdCompra() {
       novosItens.splice(indice, 1);
       setItens(novosItens);
     }
-    calculaTotal();
-  }
+  };
 
   const aumenta = (linha) => {
     const novosItens = [...itens];
@@ -87,8 +89,7 @@ function AdCompra() {
       novosItens[indice].quantidade++;
       setItens(novosItens);
     }
-    calculaTotal();
-  }
+  };
   const diminui = (linha) => {
     const novosItens = [...itens];
     let indice = novosItens.indexOf(linha);
@@ -100,17 +101,17 @@ function AdCompra() {
         removeProduto(linha);
       }
     }
-    calculaTotal();
-  }
+  };
 
   const calculaTotal = () => {
     let total = 0;
-    itens.map(item => {
+    itens.map((item) => {
       total += item.preco * item.quantidade;
     });
     setTotal(total);
-  }
+  };
 
+  // Modelagem da tabela de produtos
   const produtosColumns = [
     {
       field: "nome",
@@ -123,7 +124,8 @@ function AdCompra() {
       headerName: "Preço",
       type: "number",
       flex: 0.3,
-      valueFormatter: (params) => `R$ ${params.value.toFixed(2).replace('.', ',')}`,
+      valueFormatter: (params) =>
+        `R$ ${params.value.toFixed(2).replace(".", ",")}`,
       renderHeader: (params) => <strong>{params.colDef.headerName}</strong>,
     },
     {
@@ -132,15 +134,15 @@ function AdCompra() {
       type: "actions",
       flex: 0.3,
       renderCell: (params) => (
-        <button onClick={() => {
-          adicionaProduto(params.row);
-          calculaTotal();
-        }}><BsArrowRightCircleFill className="text-blue-400" size={25} /></button>
+        <button onClick={() => adicionaProduto(params.row)}>
+          <BsArrowRightCircleFill className="text-blue-400" size={25} />
+        </button>
       ),
       renderHeader: (params) => <strong>{params.colDef.headerName}</strong>,
-    }
-  ]
+    },
+  ];
 
+  // Modelagem da tabela da compra
   const compraColumns = [
     {
       field: "nome",
@@ -153,7 +155,8 @@ function AdCompra() {
       headerName: "Preço",
       type: "number",
       flex: 0.2,
-      valueFormatter: (params) => `R$ ${params.value.toFixed(2).replace('.', ',')}`,
+      valueFormatter: (params) =>
+        `R$ ${params.value.toFixed(2).replace(".", ",")}`,
       renderHeader: (params) => <strong>{params.colDef.headerName}</strong>,
     },
     {
@@ -164,19 +167,24 @@ function AdCompra() {
       renderHeader: (params) => <strong>{params.colDef.headerName}</strong>,
       renderCell: (params) => (
         <div className="flex flex-row justify-center items-center gap-3">
-          <button onClick={() => {
-            diminui(params.row);
-            calculaTotal();
-            }}><AiFillMinusCircle size={30} className="text-gray-800" /></button>
-          <span className="text-lg select-none flex justify-center items-center w-8 border-2 border-gray-500 rounded">{params.row.quantidade}</span>
-          <button onClick={() => {
-            aumenta(params.row);
-            calculaTotal();
-            }}><AiFillPlusCircle size={30} className="text-gray-800" /></button>
+          <button onClick={() => diminui(params.row)}>
+            <AiFillMinusCircle size={30} className="text-gray-800" />
+          </button>
+          <span className="text-lg select-none flex justify-center items-center w-8 border-2 border-gray-500 rounded">
+            {params.row.quantidade}
+          </span>
+          <button onClick={() => aumenta(params.row)}>
+            <AiFillPlusCircle size={30} className="text-gray-800" />
+          </button>
         </div>
-      )
+      ),
     },
-  ]
+  ];
+
+  // Re-renderização do total
+  useEffect(() => {
+    calculaTotal();
+  }, [itens]);
 
   return (
     <div
@@ -187,7 +195,9 @@ function AdCompra() {
 
       <main className="container h-4/5 rounded bg-white flex flex-col justify-start items-center p-3">
         <div className="flex justify-start items-center w-full">
-          <Link to="/compras" className="flex justify-center items-center mr-3"><BiArrowBack size={24} /></Link>
+          <Link to="/compras" className="flex justify-center items-center mr-3">
+            <BiArrowBack size={24} />
+          </Link>
           <h2 className="my-4 text-xl">Nova Compra</h2>
         </div>
         <div className="flex w-full h-5/6 gap-3">
@@ -212,8 +222,19 @@ function AdCompra() {
                 options={clientes}
                 sx={{ width: 300 }}
                 renderInput={(params) => (
-                  <div className="border-2 border-gray-500 rounded h-" ref={params.InputProps.ref}>
-                    <input style={{width: '100%', outline: "none", padding: '0.5rem', height: '2rem'}}  {...params.inputProps} />
+                  <div
+                    className="border-2 border-gray-500 rounded h-"
+                    ref={params.InputProps.ref}
+                  >
+                    <input
+                      style={{
+                        width: "100%",
+                        outline: "none",
+                        padding: "0.5rem",
+                        height: "2rem",
+                      }}
+                      {...params.inputProps}
+                    />
                   </div>
                 )}
               />
@@ -224,15 +245,19 @@ function AdCompra() {
                 columns={compraColumns}
                 rows={itens}
                 sx={{
-                  border: 0
+                  border: 0,
                 }}
               />
               <div className="space-y-2">
                 <div className="flex h-10 px-2 border-t-2 border-dotted border-t-gray-600 justify-between items-center">
                   <span className="font-bold text-lg">Total:</span>
-                  <span className="font-bold text-lg opacity-70">R$ {total.toFixed(2).replace('.', ',')}</span>
+                  <span className="font-bold text-lg opacity-70">
+                    R$ {total.toFixed(2).replace(".", ",")}
+                  </span>
                 </div>
-                <button className="bg-green-500 text-gray-100 p-2 w-full rounded">Adicionar</button>
+                <button className="bg-green-500 text-gray-100 p-2 w-full rounded">
+                  Adicionar
+                </button>
               </div>
             </div>
           </div>
