5a5432b114486a6ffe238db87b86a1a3012cbd6a
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-05-24 18:42:24 -0300

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.cantinasenai.com.br"
+  baseURL: "https://api.cantinasenai.com.br",
+  timeout: 30000
 });
 
 export default api;
