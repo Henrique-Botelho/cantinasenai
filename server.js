87213c49fd6623ac1f97aa3eb41dd7fbac478d29
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-05-29 08:54:24 -0300

    servidor

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const baseDir = `${__dirname}/dist/`;
+
+app.use(express.static(`${baseDir}`));
+
+app.get('*', (req, res) => res.sendFile('index.html', { root: baseDir }));
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}...`));
