9812294248d9e568fd066acdeaf8a32fef62567e
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-01 08:56:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer autoClose={3000} />
       <RoutesApp />
     </>
   )
