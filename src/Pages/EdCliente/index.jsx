a1ba2e629caa54cf135e7c9c05356714752e876a
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-03 20:46:41 -0300

    style: responsividade edcliente completa

--- src/Pages/EdCliente/index.jsx
+++ src/Pages/EdCliente/index.jsx
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom";
 import { MainContext } from "../../contexts";
 
 import Header from "../../components/Header";
+import Nav from "../../components/Nav";
 
 import { BiArrowBack } from "react-icons/bi";
 import { VscLoading } from "react-icons/vsc";
@@ -32,8 +33,9 @@ function EdCliente() {
       className="h-screen w-screen flex justify-center items-center"
     >
       <Header />
-      <main className="container w-96 rounded bg-white flex flex-col justify-center items-center p-10">
-        <div className="flex justify-start items-center w-full mb-5">
+      <Nav />
+      <main className="container fixed top-20 bottom-0 rounded flex flex-col justify-center items-center p-2">
+        <div className="bg-white flex justify-start items-center w-full sm:w-96 p-2 rounded-t">
           <Link
             to="/clientes"
             className="flex justify-center items-center mr-3"
@@ -42,7 +44,7 @@ function EdCliente() {
           </Link>
           <h2 className="my-4 text-xl">Editar cliente</h2>
         </div>
-        <form className="w-full">
+        <form className="w-full sm:w-96 bg-white p-2 rounded-b">
           <div className="flex flex-col mb-5">
             <span className="font-bold opacity-75 text-sm mb-2">
               Nome do cliente
