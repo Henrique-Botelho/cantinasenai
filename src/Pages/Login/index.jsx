df4adc6b0f6c3f2c29f166cc46135f279fae7ab2
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-06-22 23:21:47 -0300

    fix: loading botão de login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import { BsShop } from "react-icons/bs";
+import { VscLoading } from "react-icons/vsc";
 import { MdEmail, MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 
@@ -16,9 +17,11 @@ function Login() {
 
   const [tipoSenha, setTipoSenha] = useState("password");
 
+  const [carregando, setCarregando] = useState(false);
+
   useEffect(() => {
     if (autenticado) {
-      let lastpathuser = localStorage.getItem('lastpathuser');
+      let lastpathuser = localStorage.getItem("lastpathuser");
       if (lastpathuser) {
         navigate(lastpathuser);
       } else {
@@ -31,9 +34,13 @@ function Login() {
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-900">
       <main className="container flex flex-col overflow-y-scroll p-2">
         <div className="flex flex-col justify-center items-center h-40 sm:h-60">
-          <BsShop className="text-red-800 text-4xl sm:text-8xl"  />
-          <h1 className="text-gray-100 text-2xl sm:text-4xl my-3">Cantina SENAI</h1>
-          <span className="text-gray-100 text-sm sm:text-lg my-3">Faça login abaixo</span>
+          <BsShop className="text-red-800 text-4xl sm:text-8xl" />
+          <h1 className="text-gray-100 text-2xl sm:text-4xl my-3">
+            Cantina SENAI
+          </h1>
+          <span className="text-gray-100 text-sm sm:text-lg my-3">
+            Faça login abaixo
+          </span>
         </div>
         <form className="flex flex-col justify-center items-center gap-3 p-3 w-full">
           <div className="flex flex-row justify-start items-center bg-red-800 h-10 w-full sm:w-96 rounded">
@@ -78,13 +85,27 @@ function Login() {
               </button>
             )}
           </div>
-          <button
-            type="submit"
-            onClick={(e) => manipulaLogin(e, email, senha)}
-            className=" text-gray-900 w-full sm:w-96 bg-gray-100 h-10 rounded hover:scale-105 duration-500"
-          >
-            Entrar
-          </button>
+          {carregando ? (
+            <button
+              disabled
+              className=" text-gray-900 w-full sm:w-96 bg-gray-100 h-10 rounded flex justify-center items-center duration-500"
+            >
+              <VscLoading size={25} className="animate-spin" />
+            </button>
+          ) : (
+            <button
+              type="submit"
+              onClick={(e) => {
+                setCarregando(true);
+                manipulaLogin(e, email, senha).finally(() =>
+                  setCarregando(false)
+                );
+              }}
+              className=" text-gray-900 w-full sm:w-96 bg-gray-100 h-10 rounded hover:scale-105 duration-500"
+            >
+              Entrar
+            </button>
+          )}
           <Link
             to="/esqueci-senha"
             className="text-red-800 mt-5 hover:text-red-500 text-sm sm:text-lg"
